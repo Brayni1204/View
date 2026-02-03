6e05b723f7ccd001152fee178173d76bfcfb7822
MyCode <7183255722@untrm.edu.pe>
2026-02-03 01:12:16 -0500

    Agrega flores decorativas, corazón palpitante, ajusta fuente y evita desaparición de contenido

--- anim.js
+++ anim.js
@@ -60,21 +60,3 @@ function updateLyrics() {
 
 setInterval(updateLyrics, 1000);
 
-//funcion titulo
-// Función para ocultar el título después de 216 segundos
-function ocultarTitulo() {
-  var titulo = document.querySelector(".titulo");
-  var wrapper = document.querySelector(".content-wrapper");
-  var elementToHide = wrapper || titulo;
-
-  if (elementToHide) {
-    elementToHide.style.animation =
-      "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
-    setTimeout(function () {
-      elementToHide.style.display = "none";
-    }, 3000); // Espera 3 segundos antes de ocultar completamente
-  }
-}
-
-// Llama a la función después de 216 segundos (216,000 milisegundos)
-setTimeout(ocultarTitulo, 216000);
